8d235f161bd420ce5dda557fdd0314947b4894b5
crismoute <cristianmotaramirez92@gmail.com>
2022-06-16 21:18:34 -0500

    Se agregaron los botones a la fila, se creo el componente Boton y crearon los estilos de los botones

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,1 +1,18 @@
-import react from 'react';
+import React from 'react';
+import '../stylesheets/Boton.css';
+
+function Boton(props) {
+
+    const esOperador = valor => {
+        return isNaN(valor) && (valor != '.') && (valor != '=');
+    }
+
+    return (
+        <div
+            className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+            {props.children}            
+        </div>
+    );
+}
+
+export default Boton;
